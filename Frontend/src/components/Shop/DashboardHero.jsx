3b75af4a66cc84a94b44abd139e91ebbeb2d0fa4
subhan513 <subhancs57@gmail.com>
2026-07-07 16:21:20 +0500

    done

--- Frontend/src/components/Shop/DashboardHero.jsx
+++ Frontend/src/components/Shop/DashboardHero.jsx
@@ -67,7 +67,7 @@ const DashboardHero = () => {
       renderCell: (params) => {
         return (
           <>
-            <Link to={`/dashboard/order/${params.id}`}>
+            <Link to={`/order/${params.id}`}>
               <Button>
                 <AiOutlineArrowRight size={20} />
               </Button>
